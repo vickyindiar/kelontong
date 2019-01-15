b415fb7d525cf13f3cfc320f2b108956fd34c457
vickyindiar <vickynewonline@gmail.com>
2019-01-15 07:36:03 +0700

    add helper row table

--- resources/js/components/helper/table/RowTable.js
+++ resources/js/components/helper/table/RowTable.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react'
 
 export class RowTable extends Component {
   render() {
-    const cellL = Object.keys(this.props.data).map((v, i) => ( <td key={i}> {
-        this.props.data[v] 
-     } </td> ))
+    const cellL = Object.keys(this.props.data).map((v, i) => ( <td key={i}> { this.props.data[v]  } </td> ))
     return (
         <tr key = {this.props.id}>
             {
